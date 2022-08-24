b9d11b566891ca95c8eebf123b0556048b6c5d2f
Hameem Md Sayem <111680116+hameemdsayem@users.noreply.github.com>
2022-08-24 20:58:46 +0600

    Calculator
    8/24/2022

---
+++ calculator.java
@@ -0,0 +1,33 @@
+package hackerrank;
+import java.util.*;
+public class code1{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double a, b;
+        int operator;
+        
+        System.out.println("Please enter 1st number: ");
+        a = input.nextDouble();
+        System.out.println("Please enter 2nd number");
+        b = input.nextDouble();
+        
+        System.out.println("Please enter number for following operation: 1. Plus; 2. Minus; 3. Division; 4. Multiplication");
+        operator = input.nextInt();
+        
+        switch (operator){
+            case 1:
+                System.out.println(a + " + " + b + " = " + (a+b));
+                break;
+            case 2:
+                System.out.println(a + " - " + b + " = " + (a-b));
+                break;
+            case 3:
+                System.out.println(a + " / " + b + " = " + (a%b));
+                break;
+            case 4:
+                System.out.println(a + " x " + b + " = " + (a*b));
+                break;
+        }
+}
+}
