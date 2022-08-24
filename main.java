e947efb0363769c1db5a64205fb822f6e91f1b97
Hameem Md Sayem <111680116+hameemdsayem@users.noreply.github.com>
2022-08-24 20:32:41 +0600

    Cube
    8/24/2022

---
+++ main.java
@@ -0,0 +1,13 @@
+package hackerrank;
+import java.util.*;
+public class code1{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int index;
+        
+        for(index = 1; index<= n; index++){
+            System.out.println("The number is: " + index + " and the cube of " + index + " is: " + (index*index*index));
+        }
+    }
+}
