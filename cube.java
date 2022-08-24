c803beef7433152a9c4abc09a1238cf67020b433
Hameem Md Sayem <111680116+hameemdsayem@users.noreply.github.com>
2022-08-24 20:36:29 +0600

    Update and rename Main.java to cube.java

--- cube.java
+++ cube.java
@@ -1,6 +1,6 @@
 package hackerrank;
 import java.util.*;
-public class Main{
+public class cube{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
